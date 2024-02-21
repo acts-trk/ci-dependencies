0d99b78adc0b85d872d04bb18e3182defbe8a791
Paul Gessinger <hello@paulgessinger.com>
2024-02-21 16:11:18 +0100

    upgrade

--- dependencies.sh
+++ dependencies.sh
@@ -4,6 +4,10 @@ set -e
 # @TODO Only on macos
 
 
+brew upgrade
 brew install ccache xerces-c eigen boost 
 brew reinstall cmake
-brew unlink r || true
+
+if [ $(brew list r) -eq 1 ]; then
+  brew unlink r
+fi

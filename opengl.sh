fde5659b2fea2414a29daf54f564535343dd55e3
Paul Gessinger <hello@paulgessinger.com>
2024-11-03 14:31:30 +0100

    indentation

--- opengl.sh
+++ opengl.sh
@@ -12,23 +12,23 @@ if [ "$os" == "ubuntu" ]; then
   sudo apt-get update
   sudo apt-get install -y libgl1-mesa-dev
 cat <<EOF > $packages_file
-    packages:
-      opengl:
-        buildable: false
-        externals:
-        - prefix: /usr/
-          spec: opengl@4.5
+packages:
+  opengl:
+    buildable: false
+    externals:
+    - prefix: /usr/
+      spec: opengl@4.5
 EOF
 cat $packages_file
 elif [ "$os" == "almalinux" ]; then
   dnf install -y mesa-libGLU
 cat <<EOF > $packages_file
-    packages:
-      opengl:
-        buildable: false
-        externals:
-        - prefix: /usr/
-          spec: opengl@4.6
+packages:
+  opengl:
+    buildable: false
+    externals:
+    - prefix: /usr/
+      spec: opengl@4.6
 EOF
 cat $packages_file
 fi

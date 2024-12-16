4000023acb6f7be3928337af5ee3cd53b53cd8f3
Paul Gessinger <hello@paulgessinger.com>
2024-12-16 11:51:32 +0100

    opengl conditional

--- opengl.sh
+++ opengl.sh
@@ -21,13 +21,23 @@ echo "OS: $os"
 if [[ "$os" == *ubuntu* ]]; then
   ${SUDO} apt-get update
   ${SUDO} apt-get install -y libgl1-mesa-dev
+
+if [[ "$os" == *ubuntu24* ]]; then
+  version="4.6"
+elif [[ "$os" == *ubuntu20* ]]; then
+  version="4.5"
+else
+  echo "Unknown OS version, default OpenGL version"
+  version="4.5"
+fi
+
 cat <<EOF > "$packages_file"
 packages:
   opengl:
     buildable: false
     externals:
     - prefix: /usr/
-      spec: opengl@4.5
+      spec: opengl@${version}
 EOF
 cat "$packages_file"
 elif [[ "$os" == *almalinux* ]]; then
